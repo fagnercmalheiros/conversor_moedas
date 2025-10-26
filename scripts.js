43e0ded3697e36b0c4822ae4347407fb08b796e1
fagnercmalheiros <fagnercmalheiros@gmail.com>
2025-10-26 17:17:00 -0300

    try catch finalizado

--- scripts.js
+++ scripts.js
@@ -8,6 +8,10 @@ const GBP = 7.22
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
 
 // manitpular o imput de amount para receber só número
 
@@ -25,15 +29,15 @@ form.onsubmit = (event) => {
     //console.log(currency.value)
     switch(currency.value){
         case "USD":
-            convertCurrency(amount.value, USD, "US$")
+            convertCurrency(Number(amount.value), USD, "US$")
         break
     
         case "EUR":
-            convertCurrency(amount.value, EUR, "¢")
+            convertCurrency(Number(amount.value), EUR, "¢")
         break
 
         case "GBP":
-            convertCurrency(amount.value, GBP, "£")
+            convertCurrency(Number(amount.value), GBP, "£")
         break
         }
 
@@ -45,8 +49,33 @@ form.onsubmit = (event) => {
 function convertCurrency(amount , price , symbol){
     //console.log(amount,price,symbol)
     try {
+        // mostra a cotação da moeda selecionada
+        description.textContent =  `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+        let total = Number(amount.value) * price
+    
+
+        if(Number.isNaN(total))
+        {
+            throw new Error("O valor informado não é um número")
+        }
         
+
+        result.textContent = `${formatCurrencyBRL(total).replace("R$", "")} Reais`
+
+        //aplica a classe para exibir o footer mostrando o resultado
+        footer.classList.add("show-result")
     } catch (error) {
-        
+        console.log(error)
+        footer.classList.remove("show-result")
+        alert("Não foi possível converter. Tente novamente mais tarde!")
     }
+}
+
+//formata a moeda em real brasileiro
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
 }
